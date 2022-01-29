591e472467f5de2d9e47f687361168b695e9c3c0
Reinaldo Arsenio Lorenzo Trujillo <reiarseni@gmail.com>
2022-01-29 08:49:22 -0500

    Add build folder to .gitignore && fix duplicate react import

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
 
-import React from "react";
-
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
